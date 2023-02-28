8601cb6143fc3f2b84bbd8e0414ef6468b50f66c
subhamgaggar <126546794+subhamgaggar@users.noreply.github.com>
2023-02-28 12:02:50 +0530

    Create assignment 2.2.java

---
+++ assignment 2.2.java
@@ -0,0 +1,22 @@
+
+package learnjava;
+public class practicejava {
+    public static void main(String[] args) {
+        int[] nums = {10, 3, 1, 16, 24};
+        int index = findNearestNeighbors(nums);
+        System.out.println("Index of the first number: " + index);
+    }
+    public static int findNearestNeighbors(int[] nums) {
+        int minDiff = Integer.MAX_VALUE;
+        int index = 0;
+        for (int i = 1; i < nums.length; i++) {
+            int diff = Math.abs(nums[i] - nums[i-1]);
+            if (diff < minDiff) {
+                minDiff = diff;
+                index = i-1;
+            }
+        }
+        return index;
+    }
+
+}
