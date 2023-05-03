69b94905b2778d5c21d2a59e186e00e3a4d48eb7
subhamgaggar <126546794+subhamgaggar@users.noreply.github.com>
2023-05-03 08:51:27 +0530

    Add files via upload

---
+++ Assignment 4/Card.java
@@ -0,0 +1,34 @@
+package sem4_java;
+import java.util.*;
+
+public class Card {
+    Scanner sc = new Scanner(System.in);
+    private String suit;
+    private String value;
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Card(String suit, String value) {
+        this.suit=suit;
+        this.value=value;
+    }
+
+    public String toString(){
+        return "\n Value of card is "+value+" of "+suit+".";
+    }
+
+}
