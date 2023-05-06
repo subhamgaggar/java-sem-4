c15ceb857f312350dac0f17ad947c8dc62fe14a3
subhamgaggar <126546794+subhamgaggar@users.noreply.github.com>
2023-05-06 13:30:35 +0530

    Add files via upload

---
+++ Assignment8/FactorialException.java
@@ -0,0 +1,20 @@
+package sem4_java;
+
+
+class FactorialException extends Exception {
+
+    private int inputNumber;
+
+    public FactorialException(String message, int inputNumber) {
+        super(message);
+        this.inputNumber = inputNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "FactorialException: " + getMessage() + " Input number: " + inputNumber;
+    }
+}
+
+
+
