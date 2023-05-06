08f4f03fde9cfbb86418e97be4545711de5cddce
subhamgaggar <126546794+subhamgaggar@users.noreply.github.com>
2023-05-06 13:56:00 +0530

    Add files via upload

---
+++ Assignment10/MyThread.java
@@ -0,0 +1,21 @@
+package sem4_java;
+
+public class MyThread extends Thread{
+
+    public void run() {
+        System.out.println("Thread is running.");
+
+        // Sleep for 1 second
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Thread is done.");
+    }
+
+}
+
+
+
